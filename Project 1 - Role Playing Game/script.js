8666b17e57351fb82a340be1bee1290f9bbaac86
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-26 14:22:51 +0400

    added logic for goFight function + linked it to update(locations[3]) function and uses props from monsters array + change text displayed to user

--- Project 1 - Role Playing Game/script.js
+++ Project 1 - Role Playing Game/script.js
@@ -163,7 +163,13 @@ function fightDragon() {
 	fighting = 2;
 	goFight;
 }
-function goFight() {}
+function goFight() {
+	update(locations[3]);
+	monsterHealth = monsters[fighting].health;
+	monsterStats.style.display = 'block';
+	monsterName.innerText = monsters[fighting].name;
+	monsterHealthText.innerText = monsterHealth;
+}
 
 function attack() {}
 function dodge() {}

ec092995f9eae7ac623f89b346b49f3a9f78d100
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-26 11:17:33 +0400

    push newWeapon to inventory array + added new if/else to correspond to number of objects/weapons in weapons array (array.length)

--- Project 1 - Role Playing Game/script.js
+++ Project 1 - Role Playing Game/script.js
@@ -96,13 +96,22 @@ function buyHealth() {
 	}
 }
 function buyWeapon() {
-	if (gold >= 30) {
-		gold -= 30;
-		currentWeapon++;
-		goldText.innerText = gold;
-		//inform user what weapon they bought
-		let newWeapon = weapons[currentWeapon].name;
-		text.innerText = `You now have a ${newWeapon}.`;
+	//condition to check there are still weapons in the array to buy
+	if (currentWeapon < weapons.length - 1) {
+		if (gold >= 30) {
+			gold -= 30;
+			currentWeapon++;
+			goldText.innerText = gold;
+			//inform user what weapon they bought
+			let newWeapon = weapons[currentWeapon].name;
+			text.innerText = `You now have a ${newWeapon}.`;
+			inventory.push(newWeapon);
+			text.innerText += ` In your inventory you have: ${inventory}.`;
+		} else {
+			text.innerText = 'You do not have enough gold to buy a weapon.';
+		}
+	} else {
+		text.innerText = 'You already have the most powerful weapon!';
 	}
 }
 function fightDragon() {}

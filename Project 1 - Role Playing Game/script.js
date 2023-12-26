adc5e126aaa14145da880da8a2ba79167b70905c
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-26 08:38:29 +0400

    created locations array variable with 2 objects and k/v pairs+ added an update(location) function

--- Project 1 - Role Playing Game/script.js
+++ Project 1 - Role Playing Game/script.js
@@ -5,6 +5,24 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ['stick', 'dagger', 'sword'];
+const locations = [
+	{
+		name: 'town square',
+		'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+		'button functions': [goStore, goCave, fightDragon],
+		text: 'You are in the town square. You see a sign that says "Store".',
+	},
+	{
+		name: 'store',
+		'button text': [
+			'Buy 10 health (10 gold)',
+			'Buy weapon (30 gold)',
+			'Go to town square',
+		],
+		'button functions': [buyHealth, buyWeapon, goTown],
+		text: 'You enter the store.',
+	},
+];
 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
@@ -46,3 +64,5 @@ function buyHealth() {}
 function buyWeapon() {}
 function goCave() {}
 function fightDragon() {}
+
+function update(location) {}

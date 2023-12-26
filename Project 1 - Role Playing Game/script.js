0eef2dcc023c717d574cfaab30011645978761cd
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-26 20:42:37 +0400

    attack( ) - added new if statement with condition the value of calling new function isMonsterHit( ) and moved monsterHealth assignment into if block

--- Project 1 - Role Playing Game/script.js
+++ Project 1 - Role Playing Game/script.js
@@ -200,8 +200,13 @@ function attack() {
 	// health -= monsters[fighting].level;
 	//to make game more interesting changed health to
 	health -= getMonsterAttackValue(monsters[fighting].level);
-	monsterHealth -=
-		weapons[currentWeapon].power + (Math.floor(Math.random() * xp) + 1);
+	//isMonsterHit() return boolean from math.random > 0.2 || health < 20
+	if (isMonsterHit()) {
+		monsterHealth -=
+			weapons[currentWeapon].power + (Math.floor(Math.random() * xp) + 1);
+	} else {
+		text.innerText += ' You miss.';
+	}
 	healthText.innerText = health;
 	monsterHealthText.innerText = monsterHealth;
 	if (health <= 0) {
@@ -216,6 +221,9 @@ function getMonsterAttackValue(level) {
 	const hit = level * 5 - Math.floor(Math.random() * xp);
 	console.log(hit);
 	return hit > 0 ? hit : 0;
+}
+function isMonsterHit() {
+	return Math.random() > 0.2 || health < 20;
 }
 function dodge() {
 	text.innerText = `You dodge the attack from the ${monsters[fighting].name}.`;

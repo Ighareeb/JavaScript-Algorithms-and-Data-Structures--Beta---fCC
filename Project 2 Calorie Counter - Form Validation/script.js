15f275b418fab94addff620e6f448e6b4831bb81
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-27 20:03:39 +0400

    added logic for addEntry( ) - imp--> creating the dynamic HTML string

--- Project 2 Calorie Counter - Form Validation/script.js
+++ Project 2 Calorie Counter - Form Validation/script.js
@@ -8,6 +8,7 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 //
 let isError = false;
+//
 // Even though you set an input element to be a number, JavaScript receives a string value. You need to write a function to clean the string value and ensure you have a number.
 function cleanInputString(str) {
 	// Note that you need to use the \ to escape the +
@@ -19,10 +20,30 @@ function cleanInputString(str) {
 	const regex = /[+-\s]/g;
 	return str.replace(regex, '');
 }
-
+//
 // In HTML, number inputs allow for exponential notation (such as 1e10). You need to filter those out. (note: e for exponential which can only appear between two numbers)
 // The + modifier matches pattern that occurs one or more times.
 function isInvalidInput(str) {
 	const regex = /\d+e\d+/i;
 	return str.match(regex);
+}
+//
+function addEntry() {
+	// assigned targetId = '#' + entryDropdown.value to determine which category the entry needs to be added to (use to search for #id)
+	let targetId = '#' + entryDropdown.value;
+	// match targetId with input-container el
+	//  === (`#${entryDropdown.value} .input-container`);
+	const targetInputContainer = document.querySelector(
+		`${targetId} .input-container`,
+	);
+	//number entries a user adds (qSA returns nodelist -  length property of NodeList === number entries)
+	const entryNumber =
+		targetInputContainer.querySelectorAll('input[type="text"]').length;
+	// build dynamic HTML string to add //template literal also allows me to start on a new line for each HTML tag/el being created
+	const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" placeholder="Calories" min="0"/>`;
+	targetInputContainer.innerHTML += HTMLString;
 }

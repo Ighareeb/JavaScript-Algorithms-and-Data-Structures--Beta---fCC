c19c4fa1210d510a1e4a262dab510eba7e2c50d6
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-27 15:29:25 +0400

    added function to filter input numbers (isInvalidInput

--- Project 2 Calorie Counter - Form Validation/script.js
+++ Project 2 Calorie Counter - Form Validation/script.js
@@ -1,3 +1,4 @@
+//Variables assigned to DOM elements
 const mainTitleElement = document.getElementById('title');
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
@@ -17,4 +18,11 @@ function cleanInputString(str) {
 	// g flag, which stands for "global", will tell the pattern to continue looking after it has found a match
 	const regex = /[+-\s]/g;
 	return str.replace(regex, '');
+}
+
+// In HTML, number inputs allow for exponential notation (such as 1e10). You need to filter those out. (note: e for exponential which can only appear between two numbers)
+// The + modifier matches pattern that occurs one or more times.
+function isInvalidInput(str) {
+	const regex = /\d+e\d+/i;
+	return str.match(regex);
 }

aa154508f810181cd1d0984fee430df690f73bfe
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-28 22:09:50 +0400

    added delete functionality (mange removal from playlist, and create a reset playlist button + added onclick attribute for delete button in renderSongs( ) function + addEventListener for resetButton

--- Project 3 Music Player -Str+Arr methods/script.js
+++ Project 3 Music Player -Str+Arr methods/script.js
@@ -143,6 +143,36 @@ const shuffle = () => {
 	setPlayerDisplay();
 	setPlayButtonAccessibleText();
 };
+//function to delete songs from playlist + add reset playlist button
+const deleteSong = (id) => {
+	if (userData?.currentSong?.id === id) {
+		userData.currentSong = null;
+		userData.songCurrentTime = 0;
+		pauseSong();
+		setPlayerDisplay();
+	}
+	userData.songs = userData?.songs.filter((song) => song.id !== id);
+	// re-render the songs, highlight it and set the play button's accessible text since the song list will change.
+	renderSongs(userData?.songs);
+	highlightCurrentSong();
+	setPlayButtonAccessibleText();
+	//check if playlist is empty --> create resetButton to reset userData object to original state
+	if (userData.songs.length === 0) {
+		const resetButton = document.createElement('button');
+		const resetText = document.createTextNode('Reset Playlist');
+		resetButton.id = 'reset';
+		resetButton.ariaLabel = 'Reset playlist';
+		resetButton.appendChild(resetText);
+		playlistSongs.appendChild(resetButton);
+		//eventListener for resetButton
+		resetButton.addEventListener('click', () => {
+			userData.songs = [...allSongs];
+			renderSongs(userData?.songs);
+			setPlayButtonAccessibleText();
+			resetButton.remove();
+		});
+	}
+};
 //function to display current song title & artist in the player-display > p
 const setPlayerDisplay = () => {
 	const playingSong = document.getElementById('player-song-title');
@@ -175,7 +205,7 @@ const renderSongs = (array) => {
 		<span class="playlist-song-artist">${song.artist}</span>
 		<span class="playlist-song-duration">${song.duration}</span>
 	</button>
-	<button class="playlist-song-delete" aria-label="Delete ${song.title}">
+	<button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
 	<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
 	</button>
 	</li>`;
@@ -211,5 +241,6 @@ nextButton.addEventListener('click', playNextSong);
 previousButton.addEventListener('click', playPreviousSong);
 //eventListener for shuffleButton --> shuffle() function
 shuffleButton.addEventListener('click', shuffle);
+
 //Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined
 renderSongs(userData?.songs);

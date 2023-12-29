710142430cbec0c7ee08201fdc82132588e71a44
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-29 06:20:35 +0400

    declared/assigned variables for DOM elements + created date object using Date constructor + used methods to declare/assign variables to date/time formats

---
+++ Project 4 - using Date object/script.js
@@ -0,0 +1,12 @@
+// Assign variables for DOM elements
+const currentDateParagraph = document.getElementById('current-date'); //<p>
+const dateOptionsSelectElement = document.getElementById('date-options'); //<select>
+//create date object using constructor
+const date = new Date();
+// console.log(date); Fri Dec 29 2023 06:11:32 GMT+0400 (Gulf Standard Time)
+// Date object methods (get date, time in different formats)
+const day = date.getDate(); //# 1-31
+const month = date.getMonth() + 1; //# 0-11 *{zero indexed so use + 1}*
+const year = date.getFullYear(); //#yyyy
+const hours = date.getHours(); //#0-23 {zero indexed; 0 = midnight}
+const minutes = date.getMinutes(); //#0-59 {starts from 0}

2369ce8a63f5f542f8c98a415efc06cc8d42d8ea
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-29 09:30:05 +0400

    COMPLETED - completed JS - added regex to filter only chars/letters/numbers & used Array methods to split, reverse, join to compare/check userInput

--- Project 5 - Palindrome Checker/script.js
+++ Project 5 - Palindrome Checker/script.js
@@ -8,10 +8,20 @@ checkBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 	const userInput = textInput.value;
 	// console.log(userInput.length);
+	const cleanedInput = userInput.replace(/[^a-zA-Z0-9]/g, '');
 	if (userInput === '') {
 		alert('Please input a value');
 	} else if (userInput.length === 1) {
 		resultEl.classList.remove('hidden');
 		resultEl.innerHTML = `<p class='user-input'><strong>${userInput}</strong> is a palindrome</p>`;
+	} else {
+		const reverse = cleanedInput.split('').reverse().join('');
+		if (cleanedInput.toLowerCase() === reverse.toLowerCase()) {
+			resultEl.classList.remove('hidden');
+			resultEl.innerHTML = `<p class='user-input'><strong>${userInput}</strong> is a palindrome</p>`;
+		} else {
+			resultEl.classList.remove('hidden');
+			resultEl.innerHTML = `<p class='user-input'><strong>${userInput}</strong> is not a palindrome</p>`;
+		}
 	}
 });

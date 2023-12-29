284379f223fb5950a05aadc6a45ea244da074503
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-29 20:12:35 +0400

    declared/assigned variables for DOM elements

---
+++ Project 7 - ToDo App_localStorage/script.js
@@ -0,0 +1,14 @@
+//declare/assign variables for DOM elements
+const taskForm = document.getElementById('task-form');
+const confirmCloseDialog = document.getElementById('confirm-close-dialog');
+const tasksContainer = document.getElementById('tasks-container');
+//user input
+const titleInput = document.getElementById('title-input');
+const dateInput = document.getElementById('date-input');
+const descriptionInput = document.getElementById('description-input');
+//Btns
+const openTaskFormBtn = document.getElementById('open-task-form-btn');
+const closeTaskFormBtn = document.getElementById('close-task-form-btn');
+const addOrUpdateTaskBtn = document.getElementById('add-or-update-task-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const discardBtn = document.getElementById('discard-btn');

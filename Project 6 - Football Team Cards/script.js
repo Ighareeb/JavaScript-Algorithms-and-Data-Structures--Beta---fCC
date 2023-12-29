2bde44f36a14aa41f38633b5862be8678d6547af
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-29 11:21:16 +0400

    Assigned/Declared variables for DOM elements

---
+++ Project 6 - Football Team Cards/script.js
@@ -0,0 +1,10 @@
+//Assign variables for DOM elements
+// 1. team-stats Els
+const teamName = document.getElementById('team');
+const typeOfSport = document.getElementById('sport');
+const worldCupYear = document.getElementById('year');
+const headCoach = document.getElementById('head-coach');
+// 2. player info cards
+const playerCards = document.getElementById('player-cards');
+// 3. dropdown menu list <select>
+const playersDropdownList = document.getElementById('players');

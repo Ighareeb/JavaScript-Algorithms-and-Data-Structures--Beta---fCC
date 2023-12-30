b1d429411e9890f7d3f1caa9e1c7ad81a1e1e61d
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-30 14:22:15 +0400

    contd. checkUserInput function completing logic

--- Project 8 - Decimal to Binary Calculator - Recursion/script.js
+++ Project 8 - Decimal to Binary Calculator - Recursion/script.js
@@ -4,7 +4,13 @@ const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 // function to check user input when it is submitted
 const checkUserInput = () => {
-	console.log(numberInput.value);
+	// console.log(numberInput.value);
+	// input type="number" element allows special characters like ., +, and e, users can input floats like 2.2, equations like 2e+3, or even just e, which you don't want to allow.
+	// parseInt() function, which converts a string into an integer or whole number - returns either an integer or NaN
+	if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+		alert('Please provide a decimal number');
+		return;
+	}
 };
 //eventListeners for submitting user input
 convertBtn.addEventListener('click', checkUserInput);

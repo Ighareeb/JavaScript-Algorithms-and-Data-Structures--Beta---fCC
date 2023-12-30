ade501dcd285e25f732a7d1be71d7b43c3f698e9
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-30 14:25:47 +0400

    added function decimalToBinary(input)

--- Project 8 - Decimal to Binary Calculator - Recursion/script.js
+++ Project 8 - Decimal to Binary Calculator - Recursion/script.js
@@ -3,15 +3,18 @@ const numberInput = document.getElementById('number-input');
 const convertBtn = document.getElementById('convert-btn');
 const result = document.getElementById('result');
 // function to check user input when it is submitted
+//FUNCTIONS
 const checkUserInput = () => {
-	// console.log(numberInput.value);
+	decimalToBinary(parseInt(numberInput.value));
 	// input type="number" element allows special characters like ., +, and e, users can input floats like 2.2, equations like 2e+3, or even just e, which you don't want to allow.
 	// parseInt() function, which converts a string into an integer or whole number - returns either an integer or NaN
 	if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
 		alert('Please provide a decimal number');
 		return;
 	}
+	numberInput.value = '';
 };
+const decimalToBinary = (input) => {};
 //eventListeners for submitting user input
 convertBtn.addEventListener('click', checkUserInput);
 numberInput.addEventListener('keydown', (e) => {

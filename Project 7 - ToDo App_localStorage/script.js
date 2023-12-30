b4b34f46a7d0ab9fd79bc2786085e0f680a8d6fe
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-30 07:52:42 +0400

    added eventListener for taskForm + addOrUpdateTaskBtn submit to save user input into taskData array & display on page + reusable function to clear all input fields (reusability) and replaced toggle in event listeners with reset( ) function + added conditional to display cancel/discard btns only if text present in input fields (closeTaskForm eventListener)

--- Project 7 - ToDo App_localStorage/script.js
+++ Project 7 - ToDo App_localStorage/script.js
@@ -17,17 +17,65 @@ const discardBtn = document.getElementById('discard-btn');
 const taskData = [];
 //currentTask object to track/manage state
 let currentTask = {};
-//eventListeners to open/close form (modal) + cancelBtn + discardBtn
+//EVENT LISTENERS:
+//1. eventListeners to open/close form (modal) + cancelBtn + discardBtn
 openTaskFormBtn.addEventListener('click', () =>
 	taskForm.classList.toggle('hidden'),
 );
 // showModal() is a method associated with the HTML dialog element. It is used to display a modal dialog box on a web page. This will display a modal with the Discard and Cancel buttons.
 closeTaskFormBtn.addEventListener('click', () => {
 	confirmCloseDialog.showModal();
+	//show cancel/discard buttons only if text is present in input fields
+	const formInputsContainValues =
+		titleInput.value || dateInput.value || descriptionInput.value;
+	if (formInputsContainValues) {
+		confirmCloseDialog.showModal();
+	} else {
+		reset();
+	}
 });
 // close() is a method of the window object you can use to close the current window, or a modal you create with the dialog element.
 cancelBtn.addEventListener('click', () => confirmCloseDialog.close());
 discardBtn.addEventListener('click', () => {
 	confirmCloseDialog.close();
-	taskForm.classList.toggle('hidden');
+	reset();
 });
+//2. eventListeners for submitting user form data
+taskForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	//check if task exists or not to decide whether to add or update task (findIndex()returns index if exists or -1)
+	const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+	//ID = titleInput value --> lowercase --> split str into array on space between words --> hyphenate when joining back into str + Date.now() to make id more unique
+	const taskObj = {
+		id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
+		title: titleInput.value,
+		date: dateInput.value,
+		description: descriptionInput.value,
+	};
+	//
+	if (dataArrIndex === -1) {
+		taskData.unshift(taskObj);
+	}
+	//display taskData in tasksContainer + added delete & edit button for each task
+	taskData.forEach(
+		({ id, title, date, description }) =>
+			(tasksContainer.innerHTML += `
+    <div class="task" id="${id}">
+        <p><strong>Title:</strong> ${title}</p>
+        <p><strong>Date:</strong> ${date}</p>
+        <p><strong>Description:</strong> ${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+    </div>
+    `),
+	);
+	reset();
+});
+//function to reset all input fields
+const reset = () => {
+	titleInput.value = '';
+	dateInput.value = '';
+	descriptionInput.value = '';
+	taskForm.classList.toggle('hidden');
+	currentTask = {};
+};

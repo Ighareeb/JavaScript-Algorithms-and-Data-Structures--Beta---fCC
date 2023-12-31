bb48e23e7441964f4c71a1611ae820e2dfb98e94
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-31 09:11:18 +0400

    completed - added function and completed convertBtn listener. Code not passing so need to figure out if there are bugs

--- Project 9 - Roman Numeral Calculator/script.js
+++ Project 9 - Roman Numeral Calculator/script.js
@@ -10,15 +10,45 @@ const convertBtn = document.getElementById('convert-btn');
 // });
 convertBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-	if (isNaN(inputEl.value)) {
-		outputEl.innerText = 'Please enter a valid number';
-	} else if (inputEl.value <= 0) {
-		outputEl.innerText = 'Please enter a number greater than or equal to 1';
-	} else if (inputEl.value >= 4000) {
-		outputEl.innerText = 'Please enter a number less than or equal to 3999';
+	const input = parseInt(inputEl.value);
+	if (isNaN(input)) {
+		outputEl.textContent = 'Please enter a valid number';
+	} else if (input <= 0) {
+		outputEl.textContent = 'Please enter a number greater than or equal to 1';
+	} else if (input >= 4000) {
+		outputEl.textContent = 'Please enter a number less than or equal to 3999';
+	} else {
+		outputEl.textContent = convertToRoman(input);
 	}
 });
-formEl.addEventListener('submit', (e) => {
-	e.preventDefault();
-	console.log(inputEl.value);
-});
+// formEl.addEventListener('submit', (e) => {
+// 	e.preventDefault();
+// 	console.log(inputEl.value);
+// });
+function convertToRoman(num) {
+	const romanNumerals = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1,
+	};
+	let romanNumeral = '';
+	for (let key in romanNumerals) {
+		// console.log(romanNumerals[value]);
+		let value = romanNumerals[key];
+		while (num >= value) {
+			romanNumeral += key;
+			num -= value;
+		}
+	}
+	return romanNumeral;
+}

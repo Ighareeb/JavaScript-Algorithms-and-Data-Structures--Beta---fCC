f572e8497c23a5df533b54bddbbe549822fb7f91
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2023-12-31 20:10:15 +0400

    added - using selection sort - added function + logic + moved notes for sort function to separate .txt document

--- Project 1 - Number Sorter - Algorithms/script.js
+++ Project 1 - Number Sorter - Algorithms/script.js
@@ -9,7 +9,8 @@ const sortInputArray = (event) => {
 		...document.getElementsByClassName('values-dropdown'),
 	].map((dropdown) => Number(dropdown.value));
 	//
-	const sortedValues = bubbleSort(inputValues);
+	// const sortedValues = bubbleSort(inputValues);
+	const sortedValues = selectionSort(inputValues);
 	// call function to display output using sortedValues array as argument
 	updateUI(sortedValues);
 };
@@ -21,22 +22,42 @@ const updateUI = (array = []) => {
 		outputValueNode.innerText = num;
 	});
 };
-//SORT FUNCTION - function to sort input array using bubble sort - starts at the beginning of the array and 'bubbles up' unsorted values towards the end, iterating through the array until it is completely sorted.
-//Because you need to compare elements you need to use an outer+inner(nested) for loop to iterate through every element EXCEPT the last one.
-const bubbleSort = (array) => {
+//==============================SORT FUNCTIONS================================
+// 1. BUBBLE SORT
+// const bubbleSort = (array) => {
+// 	for (let i = 0; i < array.length; i++) {
+// 		for (let j = 0; j < array.length - 1; j++) {
+// 			//!!*console.log for debugging + shows how bubble sort works*!!
+// 			console.log(array, array[j], array[j + 1]);
+// 			//check if the current element is larger than the next element. You can do this by accessing the array at j and j+1. Need to create a temp variable to swap the position of the elements (think of swapping liquids between two glasses, you need a third to store one temporarily while doing so)
+// 			if (array[j] > array[j + 1]) {
+// 				const temp = array[j];
+// 				array[j] = array[j + 1];
+// 				array[j + 1] = temp;
+// 			}
+// 		}
+// 	}
+// 	return array;
+// };
+//---------------------------------------------------
+// 2. SELECTION SORT
+const selectionSort = (array) => {
 	for (let i = 0; i < array.length; i++) {
-		for (let j = 0; j < array.length - 1; j++) {
-			//!!*console.log for debugging + shows how bubble sort works*!!
-			console.log(array, array[j], array[j + 1]);
-			//check if the current element is larger than the next element. You can do this by accessing the array at j and j+1. Need to create a temp variable to swap the position of the elements (think of swapping liquids between two glasses, you need a third to store one temporarily while doing so)
-			if (array[j] > array[j + 1]) {
-				const temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+		let minIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			//!!*console.log for debugging + shows how selection sort works*!!
+			console.log(array, array[j], array[minIndex]);
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
 			}
 		}
+		const temp = array[i];
+		array[i] = array[minIndex];
+		array[minIndex] = temp;
 	}
 	return array;
 };
+//---------------------------------------------------
+// 3. INSERTION SORT
 //test code while writing
 sortButton.addEventListener('click', sortInputArray);

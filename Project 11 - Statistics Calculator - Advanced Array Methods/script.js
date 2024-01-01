2d0da397a0518e23212b5f0d99dc777b1527ddfc
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-01 06:19:40 +0400

    added- created regexed, filtered numbers array from string user input value. need to work on statistics functions before being able to complete main calculate() function

--- Project 11 - Statistics Calculator - Advanced Array Methods/script.js
+++ Project 11 - Statistics Calculator - Advanced Array Methods/script.js
@@ -1,6 +1,16 @@
+//MAIN FUNCTION - calculate()
 const calculate = () => {
-	//get value of user input
+	//get value of user input (value of an input element is always a string, even if the input type is number)
 	const value = document.querySelector('#numbers').value;
-	//split string (value from input is a number string) returns ARRAY of strings {separating the string at each comma - followed by zero or more spaces \s*}
-	const array = value.split(/,\s*/g);
+	//split string returns ARRAY of strings {separating the string at each comma - followed by zero or more spaces \s*}
+	const arrayStr = value.split(/,\s*/g);
+	// //convert string elements of the array in numbers
+	// const numberArr = arrayStr.map((str) => Number(str));
+	// //Number() constructor return NaN if value cannot be converted to number. Use to verify/check that you are only working with user input that are numbers
+	// const filteredNumArr = numberArr.filter(num => !Number.isNaN(num));
+	//refactored by chaining array methods
+	const filteredNumArr = arrayStr
+		.map((str) => Number(str))
+		.filter((num) => !Number.isNaN(num));
 };
+//-------------------------STATISTICS FUNCTIONS------------------

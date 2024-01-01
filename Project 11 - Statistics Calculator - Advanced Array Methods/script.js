593db27a7a11646183a59b51b21032a9217728a0
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-01 07:52:48 +0400

    added - MEAN function (long + refactored form) + added to calculate( ) function <call + display output> + !* added -return false- in onsubmit attribute in HTML (like doing e.preventDefault() )

--- Project 11 - Statistics Calculator - Advanced Array Methods/script.js
+++ Project 11 - Statistics Calculator - Advanced Array Methods/script.js
@@ -12,5 +12,21 @@ const calculate = () => {
 	const filteredNumArr = arrayStr
 		.map((str) => Number(str))
 		.filter((num) => !Number.isNaN(num));
+	//------------------defined statistics functions nested in calculate()-------------------
+	//1.MEAN - call + display output
+	const mean = getMean(filteredNumArrArray);
+	document.querySelector('#mean').textContent = mean;
 };
+//
 //-------------------------STATISTICS FUNCTIONS------------------
+//1. MEAN - using .reduce(accumlator, currentValue =>{}, initialValue) method takes an array and applies a callback function to condense the array into a single value. (MEAN = [sum of all values] / [# values])
+const getMean = (array) => {
+	const sum = array.reduce((acc, el) => {
+		acc + el;
+	}, 0);
+	const mean = sum / array.length;
+	return mean;
+};
+// //refactored
+// const getMean = (array) =>
+// 	array.reduce((acc, el) => acc + el, 0) / array.length;

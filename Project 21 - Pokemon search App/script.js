2504a3fafe58ff9d357395479c779d1ca1266887
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 20:29:00 +0400

    added - cotd working on function --> display pokemon data -most tests passed

--- Project 21 - Pokemon search App/script.js
+++ Project 21 - Pokemon search App/script.js
@@ -6,15 +6,20 @@ const pid = document.getElementById('pokemon-id');
 const weight = document.getElementById('weight');
 const height = document.getElementById('height');
 const sprite = document.getElementById('sprite');
-const type = document.getElementById('type');
-//
-const url = 'https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/pikachu';
-//
+const type = document.querySelector('.type');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defence = document.getElementById('defence');
+const spAttack = document.getElementById('special-attack');
+const spDefence = document.getElementById('special-defence');
+const speed = document.getElementById('speed');
 
 //
 const getPokemon = async (query) => {
 	try {
-		const res = await fetch(`${url}`);
+		const res = await fetch(
+			`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${query}`,
+		);
 		const data = await res.json();
 		console.log(data);
 
@@ -27,7 +32,13 @@ const getPokemon = async (query) => {
 		weight.textContent = `Weight: ${data.weight}`;
 		height.textContent = `Height: ${data.height}`;
 		sprite.src = data.sprites.front_default;
-		type.textContent = data.types[0].type.name;
+		type.textContent = data.types[0].type.name.toUpperCase();
+		hp.textContent = `${data.stats[0].base_stat}`;
+		attack.textContent = `${data.stats[1].base_stat}`;
+		defence.textContent = `${data.stats[2].base_stat}`;
+		spAttack.textContent = `${data.stats[3].base_stat}`;
+		spDefence.textContent = `${data.stats[4].base_stat}`;
+		speed.textContent = `${data.stats[5].base_stat}`;
 	} catch (error) {
 		console.log('Error:', error);
 	}
@@ -37,6 +48,6 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const query = userInput.value.toLowerCase();
 	console.log(typeof query);
-	// getPokemon(query);
+	getPokemon(query);
 });
-getPokemon();
+// getPokemon();

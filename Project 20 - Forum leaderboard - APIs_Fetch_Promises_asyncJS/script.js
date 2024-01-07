80a8cfe23d230a0390ed831f9fb2258423c684ee
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 09:03:06 +0400

    added - declare/assign variables for DOM elements + URLS

---
+++ Project 20 - Forum leaderboard - APIs_Fetch_Promises_asyncJS/script.js
@@ -0,0 +1,7 @@
+// declare/assign variables for DOM
+const postsContainer = document.getElementById('posts-container');
+//variables for URLs
+const forumLatest = 'https://forum-proxy.freecodecamp.rocks/latest';
+const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
+const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
+const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';

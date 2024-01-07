c993a5d4992ff5699f294b55147d18935a109f85
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 09:09:50 +0400

    added - async_(await) function with fetch req with try/catch to handle errors

--- Project 20 - Forum leaderboard - APIs_Fetch_Promises_asyncJS/script.js
+++ Project 20 - Forum leaderboard - APIs_Fetch_Promises_asyncJS/script.js
@@ -5,3 +5,15 @@ const forumLatest = 'https://forum-proxy.freecodecamp.rocks/latest';
 const forumTopicUrl = 'https://forum.freecodecamp.org/t/';
 const forumCategoryUrl = 'https://forum.freecodecamp.org/c/';
 const avatarUrl = 'https://sea1.discourse-cdn.com/freecodecamp';
+//function to fetch data from API to populate forum leaderboard
+const fetchData = async () => {
+	try {
+		const res = await fetch(forumLatest);
+		const data = await res.json();
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+fetchData();

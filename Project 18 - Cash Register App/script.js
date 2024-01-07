6de9ee1676741b4e2b6c36aaadcd515eba67dba7
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 06:20:20 +0400

    (waiting for bugs in project 18 to be fixed before completing -  START project 19 - added HTML and CSS styles + declare/assign variables for DOM elements

--- Project 18 - Cash Register App/script.js
+++
@@ -1,6 +0,0 @@
-// declare/assign variables for DOM elements
-const cashReceived = document.getElementById('cash');
-const purchaseBtn = document.getElementById('purchase-btn');
-const changeDue = document.getElementById('change-due');
-const totalPriceScreen = document.getElementById('price-screen');
-const cashInRegister = document.getElementById('cash-drawer-display');
---
+++ Project 19 - Authors Page - APIs_Fetch_Promises_asyncJS/script.js
@@ -0,0 +1,3 @@
+//declare/assign variables for DOM elements
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');

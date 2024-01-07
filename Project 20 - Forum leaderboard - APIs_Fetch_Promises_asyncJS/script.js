97d351cea7b1b30f04e77b2c073d1fc240c78893
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 11:06:16 +0400

    added - function to display data on page (call it in try block)

--- Project 20 - Forum leaderboard - APIs_Fetch_Promises_asyncJS/script.js
+++ Project 20 - Forum leaderboard - APIs_Fetch_Promises_asyncJS/script.js
@@ -11,9 +11,40 @@ const fetchData = async () => {
 		const res = await fetch(forumLatest);
 		const data = await res.json();
 		console.log(data);
+		showLatestPosts(data);
 	} catch (err) {
 		console.log(err);
 	}
 };
 
 fetchData();
+//function to display data on page
+const showLatestPosts = (data) => {
+	const { topic_list, users } = data; //destructure - get topic_list,users props from data object.
+	const { topics } = topic_list; //Destructure topics array from the topic_list object.
+	postsContainer.innerHTML = topics
+		.map((item) => {
+			//destructure props from the item (each topic) object:
+			const {
+				id,
+				title,
+				views,
+				posts_count,
+				slug,
+				posters,
+				category_id,
+				bumped_at,
+			} = item;
+			return `
+    <tr>
+    	<td>
+        	<p class="post-title">${title}</p>
+    	</td>
+    	<td></td>
+    	<td>${posts_count - 1}</td>
+    	<td>${views}</td>
+    	<td></td>
+    </tr>`;
+		})
+		.join('');
+};

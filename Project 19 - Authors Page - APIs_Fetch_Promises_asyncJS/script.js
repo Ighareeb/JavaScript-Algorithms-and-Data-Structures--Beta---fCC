8e1b6a035a496cdbdf3c2b47101c8a31ae9fde5a
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-07 07:52:24 +0400

    added - variables to choose/store authors from fetch req + function to display data in UI (will be used in fetch req)

--- Project 19 - Authors Page - APIs_Fetch_Promises_asyncJS/script.js
+++ Project 19 - Authors Page - APIs_Fetch_Promises_asyncJS/script.js
@@ -1,11 +1,28 @@
 //declare/assign variables for DOM elements
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
-
+//variables to choose/store authors from fetch req
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+//function to display data in UI (will be used in fetch req)
+const displayAuthors = (authors) => {
+	authors.forEach(({ author, image, url, bio }, index) => {
+		authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+    <h2 class="author-name">${author}</h2>
+    <img class="user-img" src="${image}" alt="${author} avatar" />
+    <p class="bio">${bio}</p>
+    <a class="author-link" href="${url}" target="_blank">${author}'s author page</a>
+    </div>`;
+	});
+};
+//fetch request
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data);
+		authorDataArr = data;
+		displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
 	})
 	.catch((err) => {
 		console.error(`There was an error: ${err}`);
